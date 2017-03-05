499832a06dd143c6388cec27512f38dbfc52b71a
DongGu Kang <ehdrnsla3020@naver.com>
2017-03-05 21:38:48 +0900

    Update reference_in.php

--- 3_Reference/reference_in.php
+++ 3_Reference/reference_in.php
@@ -1,4 +1,6 @@
 <?php
+//Input the Data of bookmark
+
 $ka=$_POST['ka'];
 $name=$_POST['name'];
 $url=$_POST['url'];
@@ -7,24 +9,23 @@ $psw=$_POST['psw'];
 //html values
 function alert() {
 	echo "<script type='text/javascript'>;
-	      alert('Wrong Password');
-  		  document.location.href=\"Reference.html\";
-				</script>\n";
+	      alert('Wrong Password');  document.location.href=\"Reference.html\";
+		</script>\n";
 	return 0;
 }
 if($psw!='site pwd'){
   alert();
 }
 else{
-$connect=mysql_connect("localhost","id","password");
-if(!$connect){
-  die('Could not connect_I: '.mysql_error());
-}
-mysql_select_db("DBname",$connect);
-mysql_query("INSERT INTO reference (ca, name, url, des)
-VALUES ('$ka','$name','$url','$des')");
+	$connect=mysql_connect("localhost","id","password");
+	if(!$connect){
+  	die('Could not connect_I: '.mysql_error());
+	}
+	mysql_select_db("DBname",$connect);
+	mysql_query("INSERT INTO reference (ca, name, url, des)
+	VALUES ('$ka','$name','$url','$des')");
 
-mysql_close($connect);
-header('Location:./Reference.html');
-}
- ?>
+	mysql_close($connect);
+	header('Location:./Reference.html');
+	}
+?>
