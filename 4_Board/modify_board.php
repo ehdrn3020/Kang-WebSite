1c29204f5786ff302a8538ace087aba9f99cf8ce
DongGu Kang <ehdrnsla3020@naver.com>
2017-03-05 21:47:57 +0900

    Update modify_board.php

--- 4_Board/modify_board.php
+++ 4_Board/modify_board.php
@@ -3,31 +3,30 @@
   $su=$_POST['subject'];
   $title=$_POST['title'];
   $writer=$_POST['writer'];
-  //$date=$_POST['date'];
   $con=$_POST['contents'];
   $psw=$_POST['psw'];
   //html values
+  
   function alert() {
   	echo "<script type='text/javascript'>;
   	      alert('Wrong Password');
     		  document.location.href=\"write.html\";
   				</script>\n";
   	return 0;
-  }
-  if($psw!='site pwd'){
-    alert();
-  }
-  else{
-  $connect=mysql_connect("localhost","id","password");
-  if(!$connect){
-    die('Could not connect_I: '.mysql_error());
-  }
+    }
+   if($psw!='site pwd'){
+     alert();
+   }
+   else{
+    $connect=mysql_connect("localhost","id","password");
+    if(!$connect){
+       die('Could not connect_I: '.mysql_error());
+       }
 
-  mysql_select_db("DBname",$connect);
-  $query = "UPDATE board1 SET Text = '$con' WHERE Title='$title'";
-  mysql_query($query, $connect);
-
-  header('Location:./w_server.html');
-  }
+    mysql_select_db("DBname",$connect);
+    $query = "UPDATE board1 SET Text = '$con' WHERE Title='$title'";
+    mysql_query($query, $connect);
+    header('Location:./w_server.html');
+    }
 
  ?>
