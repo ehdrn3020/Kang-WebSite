5814916bd573b19c2a711c2ab10e9e79a6c67e08
DongGu Kang <ehdrnsla3020@naver.com>
2017-03-05 21:42:50 +0900

    Update look_del.php

--- 4_Board/look_del.php
+++ 4_Board/look_del.php
@@ -12,15 +12,15 @@ if($pwd!='site pwd'){
   alert();
 }
 else{
-$connect=mysql_connect("localhost","id","password");
-if(!$connect){
-  die('Could not connect_ds: '.mysql_error());
-}
-$tx=$_GET['tx'];
+	$connect=mysql_connect("localhost","id","password");
+	if(!$connect){
+  		die('Could not connect_ds: '.mysql_error());
+		}
 
-mysql_select_db("DBname",$connect);
-mysql_query("DELETE FROM board1 WHERE Title=\"$tx\"");
-mysql_close($connect);
-header('Location:./w_server.html');
-}
+	$tx=$_GET['tx'];
+	mysql_select_db("DBname",$connect);
+	mysql_query("DELETE FROM board1 WHERE Title=\"$tx\"");
+	mysql_close($connect);
+	header('Location:./w_server.html');
+	}
 ?>
